1074261d520b317a80d3660a818a1a00738e4405
Alasdair Smith <alasdairsmith100@gmail.com>
2013-03-10 22:11:44 +0000

    First (real) commit, with app setup + first controller

---
+++ controllers/links/index.js
@@ -0,0 +1,38 @@
+exports.view = 'link';
+
+exports.before = function(req, res, next) {
+	var id = req.params.link_id;
+
+	if ( ! id)
+		return next(new Error('Model not found'));
+
+	// Get model
+	next();
+
+	/*
+	
+	Alternative idea - use `before` function to set up model? (also validate/filter?)
+	Then in called function, fetch actual models - gets correct models based on call (single/all models)
+
+	 */
+};
+
+exports.getAll = function(req, res, next) {
+
+};
+
+exports.getSingle = function(req, res, next) {
+	
+};
+
+exports.postSingle = function(req, res, next) {
+
+};
+
+exports.putSingle = function(req, res, next) {
+
+};
+
+exports.deleteSingle = function(req, res, next) {
+
+};
